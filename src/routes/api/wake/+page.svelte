19304244af2bcdcc660a3bac23da8c06e5ea885a
JohnnyJTH <johanohly@gmail.com>
2023-12-12 12:59:35 +0100

    Whack shit

---
+++ src/routes/api/wake/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        await fetch("/api/wake", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                password: $page.url.searchParams.get("password"),
+                mac: $page.url.searchParams.get("mac"),
+            }),
+        });
+    });
+</script>
